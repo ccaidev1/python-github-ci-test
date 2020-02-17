d33cde8739c6efb5e14e0a445f0ffe7b8089276b
Eugene <usupovzh@gmail.com>
2020-02-17 10:47:39 +0300

    initial commit

---
+++ example_pkg/client.py
@@ -0,0 +1,9 @@
+class Client(object):
+  API_URL = 'https://example.com'
+
+  def __init__(self, api_key=None, key_id = ""):
+    self.API_KEY = api_key
+    self.keyId = key_id
+
+  def get_order_status(self, **params):
+      return "Waiting"
